441293de0109444844789d2e5323dbc6497d89de
Toadboyf <Zmartens93@gmail.com>
2020-08-31 21:21:49 -0400

    Init
    Uploading Assets and started building interior home.

---
+++ Chapter_1_ The_Plague/Assets/PlagueDoctorStuff/WalkingScript.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkingScript : MonoBehaviour
+{
+
+    Animator anim;
+    CharacterController playerController;
+    public float moveSpeed;
+    public float rotSpeed;
+    float gravity = 9.8f;
+    float vSpeed = 0;
+    public bool disabled;
+    public AudioClip[] walkSounds;
+    AudioSource audioSource;
+    AudioClip lastPlayedClip;
+    AudioClip currentClip;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+        playerController = GetComponent<CharacterController>();
+        disabled = false;
+    }
+
+    void Update()
+    {
+        if(!disabled)
+        {
+            float moveForward = Input.GetAxis("Vertical");
+            float rotTurn = Input.GetAxis("Horizontal");
+
+            Vector3 vel = transform.forward * moveForward * moveSpeed;
+
+            if(playerController.isGrounded)
+            {
+                vSpeed = 0;
+            }
+            if(rotTurn != 0)
+            {
+                transform.Rotate(transform.up, rotSpeed * rotTurn * Time.deltaTime);
+            }
+            if(moveForward != 0)
+            {
+                anim.SetBool("Walking", true);
+            }
+            else
+            {
+                anim.SetBool("Walking", false);
+            }
+
+            vSpeed -= gravity * Time.deltaTime;
+            vel.y = vSpeed;
+            playerController.Move(vel * Time.deltaTime);
+        }
+    }
+
+    void PlayRandomWalkNoise()
+    {
+        int randClip;
+        do
+        {
+            randClip = Random.Range(0, walkSounds.Length);
+            currentClip = walkSounds[randClip];
+        }
+        while(currentClip == lastPlayedClip);
+
+        audioSource.PlayOneShot(currentClip);
+        lastPlayedClip = currentClip;
+    }
+}
