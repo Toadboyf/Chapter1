717647ccd68c09816af2ad71922bb8030c231b9d
Toadboyf <Zmartens93@gmail.com>
2020-08-31 23:35:32 -0400

    more asssets

---
+++ Chapter_1_ The_Plague/Assets/Scripts/Shot.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot : MonoBehaviour
+{
+    public void CutToShot () {
+        Camera.main.transform.localPosition = transform.position;
+        Camera.main.transform.localRotation = transform.rotation;
+    }
+
+    void OnDrawGizmosSelected () {
+        if (!Application.isPlaying) {
+            CutToShot();
+        }
+    }
+}
---
+++ Chapter_1_ The_Plague/Assets/Scripts/ShotZone.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotZone : MonoBehaviour
+{
+    public Shot targetShot;
+    void OnTriggerEnter (Collider c) {
+        if (c.CompareTag("Player")) {
+            targetShot.CutToShot();
+        }
+    }
+}
